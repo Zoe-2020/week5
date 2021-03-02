a27f0b2a8fc8c9b842b4b0e2ebb2d731ee4bed84
Zoe-2020 <60262018+Zoe-2020@users.noreply.github.com>
2021-03-02 20:38:41 +0000

    Add project files.

---
+++ Q1/MainWindow.xaml.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Q1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        NORTHWNDEntities db = new NORTHWNDEntities();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnQueryEx1_Click(object sender, RoutedEventArgs e)
+        {
+            var query = from c in db.Customers
+                        select c.CompanyName;
+
+            lbxCustomersEx1.ItemsSource = query.ToList();
+        }
+
+        private void btnQueryEx2_Click(object sender, RoutedEventArgs e)
+        {
+            var query = from c in db.Customers
+                        select c;
+
+            dgCustomerEx2.ItemsSource = query.ToList();
+        }
+
+        private void btnQueryEx3_Click(object sender, RoutedEventArgs e)
+        {
+            var query = from o in db.Orders
+                        where o.Customer.City.Equals("London")
+                        || o.Customer.City.Equals("Paris")
+                        || o.Customer.Country.Equals("USA")
+                        orderby o.Customer.CompanyName
+                        select new
+                        {
+                            CustomerName = o.Customer.CompanyName,
+                            City = o.Customer.City,
+                            Address = o.ShipAddress
+                        };
+
+        }
+
+        private void btnQueryEx4_Click(object sender, RoutedEventArgs e)
+        {
+            var query = from p in db.Products
+                        where p.Category.CategoryName.Equals("Beverages")
+                        orderby p.ProductID descending
+                        select new
+                        {
+                            p.ProductID,
+                            p.ProductName,
+                            p.Category.CategoryName,
+                            p.UnitPrice
+                        };
+            dgCustomerEx4.ItemsSource = query.ToList();
+        }
+
+        private void btnQueryEx5_Click(object sender, RoutedEventArgs e)
+        {
+            Product p = new Product()
+            {
+                ProductName = "Kickapoo Jungle Joy Juice",
+                UnitPrice = 12.49m,
+                CategoryID = 1
+            };
+            db.Products.Add(p);
+            db.SaveChanges();
+
+            ShowProducts(dgCustomerEx5);
+        }
+
+        private void ShowProducts(DataGrid currentGrid)
+        {
+            var query = from p in db.Products
+                        where p.Category.CategoryName.Equals("Beverages")
+                        orderby p.ProductID descending
+                        select new
+                        {
+                            p.ProductID,
+                            p.ProductName,
+                            p.Category.CategoryName,
+                            p.UnitPrice
+                        };
+            currentGrid.ItemsSource = query.ToList();
+
+        }
+
+        private void btnQueryEx6_Click(object sender, RoutedEventArgs e)
+        {
+            Product p1 = (db.Products
+                .Where(p => p.ProductName.StartsWith("Kick"))
+                .Select(p => p)).First();
+
+            p1.UnitPrice = 100m;
+
+            db.SaveChanges();
+            ShowProducts(dgCustomerEx6);
+        }
+        private void btnQueryEx7_Click(object sender, RoutedEventArgs e)
+        {
+            var products = from p in db.Products
+                           where p.ProductName.StartsWith("Kick")
+                           select p;
+
+            foreach (var item in products)
+            {
+                item.UnitPrice = 100m;
+            }
+
+            db.SaveChanges();
+            ShowProducts(dgCustomerEx7);
+        }
+        private void btnQueryEx8_click(object sender, RoutedEventArgs e)
+        {
+            var products = from p in db.Products
+                           where p.ProductName.StartsWith("Kick")
+                           select p;
+
+            db.Products.RemoveRange(products);
+            db.SaveChanges();
+            ShowProducts(dgCustomerEx8);
+
+
+        }
+
+        private void btnQueryEx9_click(object sender, RoutedEventArgs e)
+        {
+            var query = db.Customers_By_City("london");
+            dgCustomerEx9.ItemsSource = query.ToList();
+
+
+
+        }
+         
+
+    }
+}
